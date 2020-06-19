5b756746806d89a6074e6562224067fa86b8d4d9
jihyuunn <791004_@likelion.org>
2020-06-19 11:15:19 +0900

    🛠 feat: 카드 컴포넌트 작성 및 추가

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import styled, {StyledProps} from "styled-components";
 
-const Card = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+interface Props {
+  id: number;
+  todo: string;
+  done: boolean;
+  onToggle: (event: any) => void;
+  className?: string;
 }
 
-export default Card
+const CardContainer = styled.div`
+  &.done {
+      text-decoration: line-through;
+  }
+  &.ongoing {
+      text-decoration: none;
+  }
+`;
+
+const Card: React.FC<Props> = ({ id, todo, done, onToggle}) => {
+
+  return (
+    <CardContainer onClick={e => onToggle(e)} data-id={id} className={done?'done':'ongoing'}>
+      {todo}
+    </CardContainer>
+  );
+};
+
+export default Card;
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import Card from "./Card";
 
 const Main = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  type Todo = { id: number; todo: string; done: boolean };
+  const [todoList, setTodoList] = useState<Todo[]>([
+    { id: 1, todo: "주민등록 등본 발급받기", done: false },
+    { id: 2, todo: "졸업예정 증명서 발급받기", done: false },
+  ]);
+  const onToggle = (event: any) => {
+    event.preventDefault();
+    let id = event.target.dataset.id-1;
+    const copyArray = todoList;
+    copyArray[id].done = !todoList[id].done;
+    setTodoList([...copyArray]);
+  };
+  return (
+    <div>
+      <Header />
+      {todoList.map((item, index) => (
+        <Card
+          key={index}
+          id={item.id}
+          todo={item.todo}
+          done={item.done}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Main
+export default Main;
