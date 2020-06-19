86e405267ee1c0967dad6fae9b57cfabae27fed8
jihyuunn <791004_@likelion.org>
2020-06-19 12:17:20 +0900

    🛠 feat: input컴포넌트 토글함수, 새로운 아이템 추가 함수 작성

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,24 +1,49 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Header from "./Header";
+import Input from "./Input";
 import Card from "./Card";
 
 const Main = () => {
+  const [showInput, setShowInput] = useState<boolean>(false);
+  const startInput = () => {
+    setShowInput(!showInput);
+  };
+
   type Todo = { id: number; todo: string; done: boolean };
   const [todoList, setTodoList] = useState<Todo[]>([
-    { id: 1, todo: "주민등록 등본 발급받기", done: false },
-    { id: 2, todo: "졸업예정 증명서 발급받기", done: false },
+    { id: 0, todo: "주민등록 등본 발급받기", done: false },
+    { id: 1, todo: "졸업예정 증명서 발급받기", done: false },
   ]);
   const onToggle = (event: any) => {
     event.preventDefault();
-    let id = event.target.dataset.id-1;
+    let id = event.target.dataset.id;
     const copyArray = todoList;
     copyArray[id].done = !todoList[id].done;
     setTodoList([...copyArray]);
   };
+
+  const [todoItem, setTodoItem] = useState<string>("");
+  const getTodoItem = (event: any) => {
+    setTodoItem(event.target.value);
+    console.log(event.target.value);
+  };
+  const addTodoItem = (event: any) => {
+    event.preventDefault();
+    const newId = todoList.length;
+    const newItem = {id: newId, todo: todoItem, done: false};
+    setTodoList(prev => [...prev, newItem]);
+    setTodoItem('')
+  };
   return (
     <div>
-      <Header />
+      <Header startInput={startInput} />
+      <Input
+        showInput={showInput}
+        todoItem={todoItem}
+        getTodoItem={getTodoItem}
+        addTodoItem={addTodoItem}
+      />
       {todoList.map((item, index) => (
         <Card
           key={index}
